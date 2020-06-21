28c5e08da794d32b29ea7d64bc96ef55ad67edeb
Santhosh Vinayaka <santhoshvinayaka235@gmail.com>
2020-06-21 11:39:58 +0100

    Create restRunner.java

---
+++ SOAP UI framework with java/JAVA files/SoapUI/src/Rest/restRunner.java
@@ -0,0 +1,44 @@
+package Rest;
+import java.io.IOException;
+
+import org.apache.xmlbeans.XmlException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.impl.wsdl.WsdlTestSuite;
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.model.iface.Submit.Status;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.support.SoapUIException;
+
+public class restRunner {
+	
+	@Test
+	public void SoapTest() throws XmlException, IOException, SoapUIException {
+
+		
+		// TODO Auto-generated method stub
+		
+
+				// TODO Auto-generated method stub
+				
+				//grab the project
+				WsdlProject project = new WsdlProject("C:\\Users\\Santhosh\\Desktop\\LibraryBook.xml");
+				//grab the testsuite in the project
+				WsdlTestSuite testsuite = project.getTestSuiteByName("LibraryAPI");
+				//grab the testcases present in the testsuite
+				for(int i=0;i<testsuite.getTestCaseCount();i++)
+				{
+					WsdlTestCase testcase = testsuite.getTestCaseAt(i);
+					//code to execute 
+					TestRunner runner = testcase.run(new PropertiesMap(), false);
+					Assert.assertEquals(String.valueOf(Status.FINISHED), String.valueOf(runner.getStatus()));
+
+
+					
+				}
+			}
+
+}
